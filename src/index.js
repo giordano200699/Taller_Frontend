b126a9a213db44c016cd1c52fddcf6f439e827b4
giordano200699 <giordano200699@hotmail.com>
2019-05-01 13:15:51 -0500

    Enrutador

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Movilidad from './componentesHechos/Movilidad';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Links, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+   <BrowserRouter>
+   		<div>
+   			<Route path='/index' component={App} />
+   			<Route path='/Movilidad' component={Movilidad} />
+   		</div>
+   </BrowserRouter>
+), document.getElementById('root'));
 registerServiceWorker();
